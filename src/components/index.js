421216c6409c38dfb98a1a463a89c46c78d7b95a
Carlos Bouzas <carlosbouzas1991@gmail.com>
2022-05-24 20:55:21 +0200

    subida para la revision del trabajo del dia 24/05/2022

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,8 @@
 const {TarjetaUsuario} = require("./usuarios/TarjetaUsuario.js");
 const {Anuncios} = require("./Anuncios/Anuncios.js");
-const {Carousel} = require("./Anuncios/carousel.js");
 const {NavTop} = require("./NavTop/navTop.js");
 const {Login} = require("./Login/Login.js");
 const {Register} = require("./Register/Register.js");
 const {Loader} = require("./Loader/Loader.js");
 
-export {TarjetaUsuario, Anuncios, Login, Register, Carousel, NavTop, Loader};
+export {TarjetaUsuario, Anuncios, Login, Register, NavTop, Loader};
